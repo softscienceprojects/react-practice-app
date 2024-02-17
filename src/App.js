9531463db8c5ff75ec4f39ff92de0a7dd6e40182
erin nicole johnson <erin.nicole.johnson@gmail.com>
2024-02-17 13:14:34 +0000

    load products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import Accordian from "./components/accordian";
 // import RandomColour from "./components/random-colour";
 // import StarRating from "./components/star-rating";
-import ImageSlider from "./components/image-slider/image";
+// import ImageSlider from "./components/image-slider";
+import LoadMoreButton from "./components/load-more-button";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       {/* Acordian */}
       {/* <Accordian /> */}
 
@@ -16,7 +17,10 @@ function App() {
       {/* <StarRating noOfStars={10} /> */}
 
       {/* Image slider */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} /> */}
+
+      {/* Load more button - note this and above the number in string, forms part of a url*/}
+      <LoadMoreButton url={"https://dummyjson.com/products"} limit={"10"} />
     </div>
   );
 }
